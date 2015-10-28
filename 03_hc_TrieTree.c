dcc73ec80d8fc1768097b8b7bb699feae9a79597
rdmclin2 <rdmclin2@163.com>
2015-10-28 19:48:57 +0800

    solve 03

--- 03_hc_TrieTree.c
+++ 03_hc_TrieTree.c
@@ -7,16 +7,98 @@
 ////
 //
 //#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 //
-//#define N 100000
 //#define TABLE_SIZE 26
 //
-//struct Node {
+//#define STR_SIZE 11
+//
+//
+//typedef struct NODE{
 //    char val;
 //    int cnt;
-//    Node[TABLE_SIZE];
-//};
+//    int index;
+//    struct NODE* children[TABLE_SIZE];
+//}Node;
+//
+//Node * tree = NULL;
+//
+//void insert(char * str){
+//   int i,j;
+//   if(tree == NULL){
+//       tree = (Node *)malloc(sizeof(Node));
+//       tree->cnt = 0;
+//       tree->index=0;
+//       for(i = 0; i< TABLE_SIZE ;i++){
+//           tree->children[i] = NULL;
+//       }
+//   }
+//    Node * cur;
+//    Node ** link = &tree;
+//    int length = (int)strlen(str);
+//    for(i = 0;i < length;i++){
+//        int flag = 0;
+//        cur = (*link);
+//        for(j = 0; j < cur->index;j++){
+//            if(str[i] == cur->children[j]->val ){
+//                link = & (cur -> children[j]);
+//                (*link)-> cnt ++;
+//                flag = 1;
+//                break;
+//            }
+//        }
+//        if(!flag){
+//            Node * node = (Node *)malloc(sizeof(Node));
+//            node -> cnt = 1;
+//            node -> val = str[i];
+//            node -> index = 0;
+//            for(j = 0; j< TABLE_SIZE ;j++){
+//                node->children[j] = NULL;
+//            }
+//            link = &(cur -> children[cur->index]);
+//            cur->index++;
+//            *link= node;
+//        }
+//    }
+//}
+//
+//int find(char * str){
+//    int i,j;
+//    Node * cur = tree;
+//    int length = (int)strlen(str);
+//    for(i = 0 ;i < length;i++){
+//        int flag = 0;
+//        for(j = 0; j < cur->index;j++) {
+//            if (str[i] == cur->children[j]->val){
+//                cur = cur->children[j];
+//                flag = 1;
+//                break;
+//            }
+//        }
+//        if(flag == 0){
+//            return 0;
+//        }
+//    }
+//    return cur -> cnt;
+//}
 //
 //int main(){
+//    int n,m,i;
+//    char * str = malloc(sizeof(char) *STR_SIZE);
+//
+//    while(scanf("%d\n",&n)!=EOF){
+//        for(i = 0 ;i< n;++i){
+//            gets(str);
+//            insert(str);
+////            puts(str);
+//        }
+//        scanf("%d\n", &m);
+//        for(i = 0 ;i< m; ++i){
+//            gets(str);
+////            puts(str);
+//            printf("%d\n",find(str));
+//        }
+//    }
 //    return 0;
 //}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 100Strike
 坚持100天，每天刷一题，首先刷Hihocoder上的题目:
-0. ^[#1241 : Best Route in a Grid](http://hihocoder.com/problemset/problem/1241) 
-1. ^[#1242 : MX Loves Game](http://hihocoder.com/problemset/problem/1242)
-2. ^[#1000 : A + B](http://hihocoder.com/problemset/solution/610828)
-3. [#1014 : Trie树](http://hihocoder.com/problemset/problem/1014)
-4. [#1148 : 2月29日](http://hihocoder.com/problemset/problem/1148)
+- ^[#1241 : Best Route in a Grid](http://hihocoder.com/problemset/problem/1241) 
+- ^[#1242 : MX Loves Game](http://hihocoder.com/problemset/problem/1242)
+- ^[#1000 : A + B](http://hihocoder.com/problemset/solution/610828)
+- ^[#1014 : Trie树](http://hihocoder.com/problemset/problem/1014)
+-  [#1148 : 2月29日](http://hihocoder.com/problemset/problem/1148)
