ed783a5d9cab060048f9fc1efade9fd40635ea96
rdmclin2 <rdmclin2@163.com>
2015-11-19 21:53:48 +0800

    sovle 1277

---
+++ templateC++.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Calvin on 15/11/19.
+//
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<utility>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
